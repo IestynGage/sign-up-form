9294ca2016dddb49ea4b5fcb4d2ded27d999fcf5
Iestyn Gage <Iestyn.Gage@talkout.com>
2022-06-07 23:41:05 +0100

    Add FormField

---
+++ src/Components/FormField/FormField.tsx
@@ -0,0 +1,20 @@
+import { HTMLInputTypeAttribute } from "react";
+
+export interface FormFieldProps {
+    label?:string;
+
+    inputType?: HTMLInputTypeAttribute;
+    
+    checkInput?: (formInput:string) => string;
+}
+
+/**
+ * Generic component to display a form field
+ */
+ export default function FormField({label="",inputType}: FormFieldProps) {
+
+    return (
+      <input type={inputType}> {label} </input>
+    );
+  }
+  
