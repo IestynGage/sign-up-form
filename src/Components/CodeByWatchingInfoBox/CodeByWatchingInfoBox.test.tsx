3bde4903ca75d204eba082f66db5f026db5e92df
Iestyn Gage <Iestyn.Gage@talkout.com>
2022-06-07 20:20:25 +0100

    Tidy up

--- src/Components/CodeByWatchingInfoBox/CodeByWatchingInfoBox.test.tsx
+++ src/Components/CodeByWatchingInfoBox/CodeByWatchingInfoBox.test.tsx
@@ -5,6 +5,6 @@ import { CodeByWatchingInfoBox } from "./CodeByWatchingInfoBox";
 test("can render app", () => {
   render(<CodeByWatchingInfoBox />);
 
-  const linkElement = screen.getByRole(/heading/ ,{name:/Learn to code by watching others/});
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByRole(/heading/ ,{name:/Learn to code by watching others/});
+  expect(heading).toBeInTheDocument();
 });
---
+++ src/pages/SignUpPage.test.tsx
@@ -0,0 +1,6 @@
+import { render } from '@testing-library/react';
+import SignUpPage from './SignUpPage';
+
+test('can render ', () => {
+  render(<SignUpPage />);
+});
