4d2caa7517f9ccd6b408be81e44c37e9ca8af30b
Iestyn Gage <Iestyn.Gage@talkout.com>
2022-06-08 00:05:35 +0100

    Add basic signup card

---
+++ src/Components/SignUpCard/SignUpCard.tsx
@@ -0,0 +1,25 @@
+import Button from "../Button/Button";
+import FormField from "../FormField/FormField";
+
+export default function SignUpCard() {
+  return (
+    <div>
+      <FormField label="First Name" />
+      <FormField label="Last Name" />
+      <FormField label="Email" inputType={"email"} />
+      <FormField label="Password" inputType={"password"} />
+      <Button label="Claim your free trial" />
+      <TermsAndCondition />
+    </div>
+  );
+}
+
+export function TermsAndCondition() {
+  return (
+    <p>
+      {" "}
+      By clicking the button you are agreeing to our{" "}
+      <a href="https://www.example.com"> Terms and services</a>
+    </p>
+  );
+}
